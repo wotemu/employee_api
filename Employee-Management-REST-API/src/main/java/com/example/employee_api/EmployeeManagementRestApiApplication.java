274ac6c577570cf26b3a241dd6f75b09724a11c6
workneh.tamire <workneh.tamire@epassi.com>
2026-07-30 12:27:41 +0300

    first commit

---
+++ Employee-Management-REST-API/src/main/java/com/example/employee_api/EmployeeManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.employee_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementRestApiApplication.class, args);
+	}
+
+}
